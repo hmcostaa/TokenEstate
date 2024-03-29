a5d8ed3be6d9f01bf6fc031ada7f20a1145f7bf6
Nuno Costa <nuno.mmc@gmail.com>
2024-03-29 22:58:49 +0000

    WIP: Add greenfield.
    Not currently working because env vars are not being loaded properly

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import type { NextPage } from "next";
+import { Bucket } from "~~/components/greenfield/bucket";
 
 const Home: NextPage = () => {
   return (
     <>
-      <div className="flex items-center flex-col flex-grow pt-10 bg-white"></div>
+      <div className="flex items-center flex-col flex-grow pt-10 bg-white">
+        <Bucket />
+      </div>
     </>
   );
 };
--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -1,10 +1,26 @@
 // @ts-check
 
+const _getPublicEnv = (prefix) => {
+  const envs = process.env;
+  const res = {};
+
+  Object.keys(envs).forEach((k) => {
+    if (k.startsWith(prefix)) {
+      res[k] = envs[k];
+    }
+  });
+
+  return res;
+};
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   typescript: {
     ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+  },
+  publicRuntimeConfig: {
+    ..._getPublicEnv('NEXT_PUBLIC_'),
   },
   eslint: {
     ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
